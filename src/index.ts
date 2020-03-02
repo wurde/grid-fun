1567a01d0bc8c167645bbcc895af4c8718798668
Andy Bettisworth <andy@accreu.com>
2020-03-02 10:35:36 -0600

    Concurrently

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import { LitElement, html, customElement } from "lit-element";
 @customElement("grid-fun-app")
 export class GridFunApp extends LitElement {
     render() {
-        return html`Grid Fun`
+        return html`<h1>Grid SO Fun!!!!!!!!!!!!!!!</h1>`
     }
 }
