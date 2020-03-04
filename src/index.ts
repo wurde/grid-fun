ee68f756cf8780862cabd0a0891c2a769f69a981
Andy Bettisworth <andy@accreu.com>
2020-03-04 11:12:07 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import { LitElement, html, css, customElement } from "lit-element";
 
 @customElement("grid-fun-app")
 export class GridFunApp extends LitElement {
-    static styles = css`
-      h2 {
-        color: red;
-        font-weight: bold;
-      }
-    `
-
-    render() {
-        return html`<h2>Grid Fun</h2>`
+  static styles = css`
+    h2 {
+      color: green;
+      font-weight: bold;
     }
+  `
+
+  render() {
+    return html`<h2>Grid Fun</h2>`
+  }
 }
