08f45bb3bae488ff95c75d05cc361b1640285d2a
Andy Bettisworth <andy@accreu.com>
2020-03-07 19:28:51 -0600

    Description

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Grid Fun
 
-A sample app using Web Components to explore Grid CSS.
+Exploring Grid CSS via Web Components.
--- src/sketches/20200302/index.ts
+++ src/sketches/20200302/index.ts
@@ -1,8 +1,6 @@
 import { ui_1, ui_2, ui_3, ui_4, ui_5, ui_6, ui_7, ui_8
-// ui_9
 } from "./ui-sketches-all";
 
 export default <any>{
   1: ui_1, 2: ui_2, 3: ui_3, 4: ui_4, 5: ui_5, 6: ui_6, 7: ui_7, 8: ui_8
-  // 9: ui_9
 }
